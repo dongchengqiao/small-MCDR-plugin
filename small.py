afa0e0f9e518f53bb8f357aa1b62c2c53cc2be6f
dongchengqiao <140995645+dongchengqiao@users.noreply.github.com>
2026-02-03 21:22:48 +0800

    更新至v1.2

--- small.py
+++ small.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 from mcdreforged.api.all import *
 
-import os
-import json
-import time
-import re
 PLUGIN_METADATA = {
     'id': 'small',
-    'version': '1.0',
+    'version': '1.2',
     'name': 'big! small!',
     'author': [
         'DC_Provide'
@@ -16,14 +12,13 @@ PLUGIN_METADATA = {
 }
 
 
-
-
 def on_info(server, info):
-    # PlayerInfoAPI = server.get_plugin_instance('PlayerInfoAPI')
-    i = 0
     if info.content == '!!small':
-        server.execute("attribute " + info.player + " minecraft:generic.scale base set 0.3") 
+        server.execute("execute as "+ info.player +" at @s run attribute @s minecraft:generic.scale base set 0.3")
+        server.execute('tellraw '+info.player+' {"translate":"commands.attribute.base_value.set.success","with":[{"translate":"attribute.name.generic.scale"},"'+info.player+'","0.3"]}')
     if info.content == '!!ultrasmall':
-        server.execute("attribute " + info.player + " minecraft:generic.scale base set 0.1") 
+        server.execute("execute as "+ info.player +" at @s run attribute @s minecraft:generic.scale base set 0.1")
+        server.execute('tellraw '+info.player+' {"translate":"commands.attribute.base_value.set.success","with":[{"translate":"attribute.name.generic.scale"},"'+info.player+'","0.1"]}')
     if info.content == '!!normal':
-        server.execute("attribute " + info.player + " minecraft:generic.scale base set 1") 
+        server.execute("execute as "+ info.player +" at @s run attribute @s minecraft:generic.scale base set 1") 
+        server.execute('tellraw '+info.player+' {"translate":"commands.attribute.base_value.set.success","with":[{"translate":"attribute.name.generic.scale"},"'+info.player+'","1.0"]}')
